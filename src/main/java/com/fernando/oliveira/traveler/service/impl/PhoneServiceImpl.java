33199c74154c33ad3eb5ba4d07fcc339dc6dfd20
fernandooliveira19 <fer.a.oliveira19@gmail.com>
2020-07-18 20:59:16 -0300

    Retirado metodos de conversões de DTO para objetos e Objetos para DTO

--- src/main/java/com/fernando/oliveira/traveler/service/impl/PhoneServiceImpl.java
+++ src/main/java/com/fernando/oliveira/traveler/service/impl/PhoneServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fernando.oliveira.traveler.domain.Phone;
-import com.fernando.oliveira.traveler.dto.PhoneDTO;
 import com.fernando.oliveira.traveler.repository.PhoneRepository;
 import com.fernando.oliveira.traveler.service.PhoneService;
 
@@ -19,27 +18,6 @@ public class PhoneServiceImpl implements PhoneService {
 		return phoneRepository.save(phone);
 		
 	}
-	
-	@Override
-	public PhoneDTO convertObjectToDto(Phone phone) {
 		
-		PhoneDTO phoneDTO = PhoneDTO.builder()
-								.id(phone.getId())
-								.prefix(phone.getPrefix())
-								.number(phone.getNumber())
-								.build();
-		return phoneDTO;
-	}
-
-	@Override
-	public Phone convertDtoToObject(PhoneDTO phoneDTO) {
-		Phone phone = Phone.builder()
-						.prefix(phoneDTO.getPrefix())
-						.number(phoneDTO.getNumber())
-						.build();
-		return phone;
-	}
-
-	
 
 }
