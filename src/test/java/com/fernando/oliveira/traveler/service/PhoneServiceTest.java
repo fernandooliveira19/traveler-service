6fc2cba0d78845400a13605c1c8616cfbbc5b867
fernandooliveira19 <fer.a.oliveira19@gmail.com>
2020-07-18 21:00:02 -0300

    Create PhoneServiceTest.java

---
+++ src/test/java/com/fernando/oliveira/traveler/service/PhoneServiceTest.java
@@ -0,0 +1,14 @@
+package com.fernando.oliveira.traveler.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PhoneServiceTest {
+
+	
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
